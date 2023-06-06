bd71647d1e53d743df980c85ae3368ae32c20d43
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-06-06 23:17:10 +0530

    Why need useState Hook? It needed to maintain the changed vlue and rerender the page.

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,8 +1,15 @@
 const ErrorExample = () => {
   let title = "random title";
+  const handleClickEvent = () => {
+    title = "Hello People";
+    console.log(title);
+  };
   return (
     <>
       <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClickEvent}>
+        Change title
+      </button>
     </>
   );
 };

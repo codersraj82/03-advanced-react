278684166133331405c099dfee595621bf0c9029
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-06-06 23:54:33 +0530

    commit-2: Basics of useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 //import Setup from "./tutorial/01-useState/starter/01-error-example";
 
-import Setup from "./tutorial/01-useState/starter/01-error-example";
+import Setup from "./tutorial/01-useState/starter/02-useState-basics";
 
 function App() {
   return (
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
+/*important note: 
+# Hook name statrt use word
+# component name must be statrted with uppercase ex UseStateBasic
+#hook should be in function or component body className"
+# cannot call conditionally
+*/
 
 const UseStateBasics = () => {
-  const [text, setText] = useState("random title");
+  // console.log(useState("hello people"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  //useState hook return array, hence need to destructure it.
+  const [text, setText] = useState("random title ");
   const handleClick = () => {
-    if (text === "random title") {
-      setText("hello world");
+    if (text === "random title ") {
+      setText("Hello people");
     } else {
-      setText("random title");
+      setText("random title ");
     }
   };
   return (
     <>
-      <h1>{text}</h1>
+      <h2>{text}</h2>
       <button className="btn" onClick={handleClick}>
         Change Title
       </button>

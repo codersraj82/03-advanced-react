b0da0ec79f212286d3c0f416361051874552e798
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-06-09 02:35:35 +0530

    simple useEffect example showing conditional execution of useEffect. Example of counter and update if document.title.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../src/index.css";
 //import Setup from "./tutorial/01-useState/starter/01-error-example";
 
-import Setup from "./tutorial/01-useState/starter/birthDayPoject";
+import Setup from "./tutorial/02-useEffect/starter/02-useEffect-basics";
 
 function App() {
   return (
--- src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
-  const sayHello = () => {
-    console.log('hello there');
-  };
+  //useEffect hook execute by default after  initial render or every re-render
+  // useEffect hook second parameter as array called depencency array
+  // this empty array [] indicates that useEffect runs only at initial render
+  // if array have dependency element then useEffect runs whenever change occured in dependency element.
+  useEffect(() => {
+    console.log("useEffect execute on every render");
+    if (value >= 1) {
+      // excute only for value variable have non zero value
+      document.title = `Click count ${value}`;
+    }
+  });
 
-  sayHello();
-
+  console.log("component render");
   return (
-    <div>
-      <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
+    <>
+      <h3>Counter</h3>
+      <h4>{value}</h4>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click me
       </button>
-    </div>
+    </>
   );
 };
 export default UseEffectBasics;

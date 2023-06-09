411170f39bfcfbe5963b876cf428ab22e2602512
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-06-09 06:20:02 +0530

    cleanup function to remove event listener to avoid multiple calling of useEffect on rerendering.

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setValue(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    console.log("useEffect called");
+    //event cleanup to trigger the event only once for change of size
+    return () => {
+      console.log("cleanup function for event is called");
+      window.removeEventListener("resize", checkSize);
+    };
+  }); // instead of cleanup function empty array [] second parameter can be used. but good practices is to add cleanup function. It is very usefull when dealing with mulitiple event listener in project.
 
   return (
-    <div>
-      <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
-      </button>
-    </div>
+    <>
+      <h4>Window</h4>
+      <h5>{value} PX</h5>
+    </>
   );
 };
 export default CodeExample;

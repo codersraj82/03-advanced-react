cc59b9a1ca9d96a9261ecb489bbadceda05be169
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-06-22 07:00:47 +0530

    controlled Table with name email formatted in table

---
+++ src/tutorial/06-forms/starter/01-controlled-inputs copy.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      console.log(firstName, email);
+      const person = { firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      firstName = "";
+      email = "";
+    } else {
+      console.log("Empty value");
+    }
+  };
+  return (
+    <>
+      <article>
+        <form action="" className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName" style={{ marginRight: "0.5rem" }}>
+              Name:
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email" style={{ marginRight: "0.5rem" }}>
+              Email:
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn">
+            Add Person
+          </button>
+        </form>
+      </article>
+    </>
+  );
+};
+export default ControlledInputs;

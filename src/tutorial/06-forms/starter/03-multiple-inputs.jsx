0e77990cbce5d5ef9baebcd4969d495067775647
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-06-23 06:40:47 +0530

    Multiple controlled input with form and table

--- src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,35 +1,92 @@
-const MultipleInputs = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Multiple Inputs</h4>
-        {/* name */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input type='text' className='form-input' id='name' />
-        </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
-            Email
-          </label>
-          <input type='email' className='form-input' id='email' />
-        </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
-            Password
-          </label>
-          <input type='password' className='form-input' id='password' />
-        </div>
+import React, { useState } from "react";
 
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-    </div>
+const MultipleInputs = () => {
+  const [person, setPerson] = useState({ firstName: "", age: "", email: "" });
+  const [people, setPeople] = useState([]);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPerson = { ...person, id: new Date().getTime().toString() };
+    setPeople([...people, newPerson]);
+    setPerson({ firstName: "", age: "", email: "" });
+  };
+  return (
+    <>
+      <article>
+        <form action="" className="form" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="firstName" style={{ margin: "1rem" }}>
+              Name
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={person.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="age" style={{ margin: "1rem" }}>
+              Age
+            </label>
+            <input
+              type="text"
+              name="age"
+              id="age"
+              value={person.age}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email" style={{ margin: "1rem" }}>
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={person.email}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn">
+            Add Person
+          </button>
+        </form>
+        <table className="customers">
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+          {people.map((ele, index) => {
+            const { firstName, age, email, id } = ele;
+
+            return (
+              <tr key={id} className="table">
+                <td>{id}</td>
+                <td>{firstName}</td>
+                <td>{age}</td>
+                <td>{email}</td>
+                <td>
+                  <span>
+                    <button className="btn">Edit</button>
+                    <button className="btn">Delete</button>
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </article>
+    </>
   );
 };
 export default MultipleInputs;
